02ccf91f1961f0ab6bfb8e5162202e907ab4f078
hihihi777 <qnalswkd@gmail.com>
2021-09-07 12:26:59 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
-function plus(a, b) {
-    console.log(a + b);
-}
-
-plus(35, 35);
-
+const calculator = {
+    add : function (a, b, c) {
+        console.log(a + b + c);
+    },
+    min : function (a, b, c) {
+        console.log(a - b - c);
+    },
+    div : function (a, b, c) {
+        console.log(a / b / c);
+    },
+    squ : function (a, b, c) {
+        console.log(a ** b ** c);
+    },
+    mul : function (a, b, c) {
+        console.log(a * b * c);
+    },
+};
+calculator.add(7, 7, 7);
+calculator.min(7, 7, 7);
+calculator.div(7, 7, 7);
+calculator.squ(7, 7, 7);
+calculator.mul(7, 7, 7);
