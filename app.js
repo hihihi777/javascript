8cab287811d2883e04f5952f99cf27a73ff38458
hihihi777 <qnalswkd@gmail.com>
2021-09-07 17:30:36 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,13 @@
-const calculator = {
-    plus : function (a, b) {
-        return a + b;
-    },
-    min : function (a, b) {
-        return a - b ;
-    },
-    div : function (a, b) {
-        return a / b;
-    },
-    squ : function (a, b) {
-        return a ** b;
-    },
-    mul : function (a, b) {
-        return a * b;
-    },
-};
-const plusResult = calculator.plus(2, 3);
-const minResult = calculator.min(plusResult, 10);
-const divResult = calculator.div(10, minResult);
-const squResult = calculator.squ(divResult, plusResult);
-const mulResult = calculator.mul(divResult, minResult);
+const age = parseInt(prompt("How old are you?"));
+
+if (isNaN(age) || age < 0) {
+    console.log("please write a number");
+} else if (age < 18) {
+    console.log("You are too young");
+} else if (50 > age && age > 19) {
+    console.log("Welcome");
+} else if (100 === age) {
+    console.log("wowwow");
+} else {
+    console.log("drink little bit");
+}
