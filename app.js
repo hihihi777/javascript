7a9c63112d3b65478c7b380e51e25d274f684262
hihihi777 <qnalswkd@gmail.com>
2021-09-07 15:31:26 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,22 @@
-console.log("hihihi");
-console.log("hohohohohohhohohohohoho");
+const calculator = {
+    plus : function (a, b) {
+        return a + b;
+    },
+    min : function (a, b) {
+        return a - b ;
+    },
+    div : function (a, b) {
+        return a / b;
+    },
+    squ : function (a, b) {
+        return a ** b;
+    },
+    mul : function (a, b) {
+        return a * b;
+    },
+};
+const plusResult = calculator.plus(2, 3);
+const minResult = calculator.min(plusResult, 10);
+const divResult = calculator.div(10, minResult);
+const squResult = calculator.squ(divResult, plusResult);
+const mulResult = calculator.mul(divResult, minResult);
