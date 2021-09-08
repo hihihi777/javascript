f65eb0bf8ccc698ad25a10be11a0cfc084d3c815
hihihi777 <qnalswkd@gmail.com>
2021-09-08 19:15:44 +0900

    js-clone

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const age = parseInt(prompt("How old are you?"));
+const h1 = document.querySelector("div.hello:first-child h1");
 
-if (isNaN(age) || age < 0) {
-    console.log("please write a number");
-} else if (age < 18) {
-    console.log("You are too young");
-} else if (50 > age && age > 19) {
-    console.log("Welcome");
-} else if (100 === age) {
-    console.log("wowwow");
-} else {
-    console.log("drink little bit");
+function handleTitleClick() {
+    const clickedClass = "clicked sexy-font"
+    if(h1.className === clickedClass) {
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
+    }
 }
+
+h1.addEventListener("click", handleTitleClick);
